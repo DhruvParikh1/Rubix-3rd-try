ca1a15b011962e2d44f85238c57784bb09840a6e
DhruvParikh1 <98427247+DhruvParikh1@users.noreply.github.com>
2023-05-12 21:47:51 -0400

    randomize functionality added

---
+++ randomize.js
@@ -0,0 +1,28 @@
+var randomizeButton = document.getElementById('randomize-btn');
+
+function randomizeCube() {
+    // Determine how many random rotations we want to make
+    var rotations = Math.floor(Math.random() * 40) + 20; // Random number between 20 and 60
+
+    // We want the rotations to happen quickly and not be easily followed by the human eye
+    // We can achieve this by queuing up the rotations and having them execute in quick succession
+    var i = 0;
+    var interval = setInterval(function() {
+        // Choose a random face (0 to 5)
+        var face = Math.floor(Math.random() * 6);
+
+        // Choose a random direction (clockwise or counter-clockwise)
+        var cw = Math.floor(Math.random() * 2);
+
+        // Execute the rotation
+        animateRotation(face, cw, Date.now());
+
+        i++;
+        if (i >= rotations) {
+            clearInterval(interval);
+        }
+    }, 200); // Execute a rotation every 50ms
+}
+
+// Add an event listener to the randomize button
+randomizeButton.addEventListener('click', randomizeCube);
